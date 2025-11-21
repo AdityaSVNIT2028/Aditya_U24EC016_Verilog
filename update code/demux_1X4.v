c52b75988f7fd2796b5504bc9b17a42b4f5e57a5
Aditya <u24ec016@eced.svnit.ac.in>
2025-11-21 21:33:13 +0530

    Delete update code directory

--- update code/demux_1X4.v
+++
@@ -1,9 +0,0 @@
-module demux(input  i_f,
-             input [1:0]i_s,
-            output reg a, b, c, d);
-  always @(*) begin 
-    a = (~(i_s[0]))&(~i_s[1]) & i_f;
-    b = (~i_s[1]) & (i_s[0]) & i_f;
-    ...
-  end
-endmodule
